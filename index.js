238f8f1d666bc10b44b84e88362c04fb80f2bc35
SujalShah3234 <shahc9437@gmail.com>
2021-07-20 11:45:36 +0530

    ⚡️ Cities added in world clock and stopwatch validation added

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ let id,
   minutes = 0,
   hours = 0;
 
-let timer = document.getElementById("show-time");
+let timer = document.getElementById('show-time');
 
-document.getElementById("defaultOpen").click();
+document.getElementById('defaultOpen').click();
 
 function openTabHandler(event, cityName) {
   let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
-  document.getElementById(cityName).style.display = "block";
-  event.currentTarget.className += " active";
+  document.getElementById(cityName).style.display = 'block';
+  event.currentTarget.className += ' active';
 }
 
 const startStopWatch = () => {
+  document.getElementById('sw-start').style.display = 'none';
+  document.getElementById('sw-stop').style.display = 'inline-block';
+  document.getElementById('sw-lap').style.display = 'inline-block';
+  document.getElementById('sw-reset').style.display = 'inline-block';
   seconds++;
   if (seconds >= 60) {
     seconds = 0;
@@ -33,38 +37,78 @@ const startStopWatch = () => {
   }
 
   timer.innerText =
-    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
-    ":" +
-    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
-    ":" +
-    (seconds > 9 ? seconds : "0" + seconds);
+    (hours ? (hours > 9 ? hours : '0' + hours) : '00') +
+    ':' +
+    (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') +
+    ':' +
+    (seconds > 9 ? seconds : '0' + seconds);
 
   incrementTimer();
 };
 
 const incrementTimer = () => {
-  id = setTimeout(startStopWatch, 1000);
+  clearInterval(id);
+  id = setInterval(startStopWatch, 1000);
+};
+
+const resumeStopWatch = () => {
+  incrementTimer();
+  document.getElementById('sw-stop').style.display = 'inline-block';
+  document.getElementById('sw-lap').style.display = 'inline-block';
+  document.getElementById('sw-resume').style.display = 'none';
 };
 
 const stopStopWatch = () => {
-  clearTimeout(id);
+  clearInterval(id);
+  document.getElementById('sw-stop').style.display = 'none';
+  document.getElementById('sw-lap').style.display = 'none';
+  document.getElementById('sw-resume').style.display = 'inline-block';
 };
 
 const resetStopWatch = () => {
-  clearTimeout(id);
-  timer.textContent = "00:00:00";
-  document.getElementById("laps").innerHTML = "";
+  clearInterval(id);
+  timer.textContent = '00:00:00';
+  document.getElementById('laps').innerHTML = '';
   seconds = 0;
   minutes = 0;
   hours = 0;
+  document.getElementById('sw-start').style.display = 'inline-block';
+  document.getElementById('sw-stop').style.display = 'none';
+  document.getElementById('sw-lap').style.display = 'none';
+  document.getElementById('sw-reset').style.display = 'none';
+  document.getElementById('sw-resume').style.display = 'none';
 };
 
 const onLapHandler = () => {
-  const li = document.createElement("li");
-  li.className = "sw-lap";
+  const li = document.createElement('li');
+  li.className = 'sw-lap';
   li.textContent = timer.innerText;
-  document.getElementById("laps").appendChild(li);
+  document.getElementById('laps').appendChild(li);
 };
 
-var date = new Date();
-console.log(date.toLocaleString("en-GB"));
+setInterval(() => {
+  const australia = new Date().toLocaleString('en-US', {
+    timeZone: 'Australia/Sydney',
+    timeStyle: 'medium',
+  });
+
+  const newYork = new Date().toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+    timeStyle: 'medium',
+  });
+
+  const london = new Date().toLocaleString('en-US', {
+    timeZone: 'Europe/London',
+    timeStyle: 'medium',
+  });
+
+  const japan = new Date().toLocaleString('en-US', {
+    timeZone: 'Japan',
+    timeStyle: 'medium',
+  });
+
+  document.getElementById('city_australia').textContent = australia;
+  document.getElementById('city_ny').textContent = newYork;
+  document.getElementById('city_london').textContent = london;
+  document.getElementById('city_japan').textContent = japan;
+}, 1000);
